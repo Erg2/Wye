d72357c1f850fbe6556dce9850a14f8bf64ded3d
Eben Gay <egay@creativemicro.com>
2025-04-22 23:57:24 -0400

    Updated jellyfish in FunFishLib

--- WyeUserLibraries/FunFIshLib.py
+++ WyeUserLibraries/FunFIshLib.py
@@ -10,6 +10,137 @@ class FunFishLib:
 
   class Jelly:
     mode = Wye.mode.PARALLEL
+    autoStart = True
+    dataType = Wye.dType.NONE
+    cType = Wye.cType.VERB
+    parTermType = Wye.parTermType.FIRST_FAIL
+    paramDescr =  ()
+    varDescr =  (
+        ("gObj",Wye.dType.OBJECT,None),
+        ("nSegs",Wye.dType.INTEGER,5),
+        ("body",Wye.dType.OBJECT,None),
+        ("tag",Wye.dType.STRING,""),
+        ("tCt",Wye.dType.INTEGER,0),
+        ("tSegCt",Wye.dType.INTEGER,0),
+        ("segLen",Wye.dType.INTEGER,5),
+        ("t",Wye.dType.OBJECT_LIST,[]),
+        ("tTags",Wye.dType.STRING_LIST,[]),
+        ("tgCt",Wye.dType.INTEGER,0),
+        ("cleanUpObjs",Wye.dType.OBJECT_LIST,None),
+        ("tAngle",Wye.dType.FLOAT,0.0),
+        ("segAngle",Wye.dType.FLOAT,0.0),
+        ("dAngle",Wye.dType.FLOAT,0.001),
+        ("color",Wye.dType.FLOAT_LIST,[0.5, 0.3, 0.3, 1.0]),
+        ("colorInc",Wye.dType.FLOAT_LIST,[0.001, 0.002, 0.003]),
+        ("tColorInc",Wye.dType.FLOAT_LIST,[0.1, 0.1, 0.1]),
+        ("noop",Wye.dType.INTEGER,0),)
+    codeDescr =        (
+        ("CreateJBodStream",
+          (
+            ("Code","#hang everything off central point"),
+            ("Code","from panda3d.core import NodePath"),
+            ("Code","frame.vars.gObj[0] = NodePath('JFish')"),
+            ("Code","frame.vars.gObj[0].reparentTo(render)"),
+            ("Code","from random import random"),
+            ("Code","frame.vars.gObj[0].setPos(((random()-.5)*20, (random()-.5) * 20, random()))"),
+            ("Code","frame.vars.gObj[0].setScale((.5,.5,.5))"),
+            ("Code","frame.vars.cleanUpObjs[0].append(frame.vars.gObj[0]) #"),
+            ("Code","frame.vars.body[0] = Wye3dObjsLib._ball(1.1, [0,0,0]) #"),
+            ("Code","frame.vars.body[0]._path.reparentTo(frame.vars.gObj[0])"),
+            ("Code","frame.vars.body[0].setScale((1,1,.25))"),
+            ("Code","frame.vars.body[0].setColor((frame.vars.color[0][0], frame.vars.color[0][1], frame.vars.color[0][2], 1))"),
+            ("Code","frame.vars.cleanUpObjs[0].append(frame.vars.body[0]._path) #"),
+            ("WyeCore.libs.WyeLib.makePickable",
+              ("Expr","frame.vars.tag"),
+              ("Expr","[frame.vars.body[0]._path]")),
+            ("Code","WyeCore.World.registerObjTag(frame.vars.tag[0], frame)"),
+            ("Label","Done"))),
+        ("CreateT1Stream",
+          (
+            ("Label","Wait1Cycle"),
+            ("Label","BldTentacles"),
+            ("Var=","frame.vars.tSegCt[0] = 0"),
+            ("Code","# one t list per tentacle"),
+            ("Code","frame.vars.t[0].append([])"),
+            ("Code","# base pos for this tentacle"),
+            ("Label","BldTentacle"),
+            ("Code","import math"),
+            ("Code","x = math.sin(frame.vars.tAngle[0])"),
+            ("Code","y = math.cos(frame.vars.tAngle[0])"),
+            ("Code","obj = Wye3dObjsLib._ball(.2-(frame.vars.tSegCt[0] * .05), [x,y,-.5 - (frame.vars.tSegCt[0] *.5)])"),
+            ("Code","obj._path.reparentTo(frame.vars.gObj[0])"),
+            ("Code","frame.vars.t[0][frame.vars.tCt[0]].append([obj])"),
+            ("Code","frame.vars.cleanUpObjs[0].append(obj._path)"),
+            ("Code","obj.setColor((frame.vars.color[0][0], frame.vars.color[0][1], frame.vars.color[0][2], 1))"),
+            ("Code","frame.vars.tTags[0].append(['']) # make array elem for tag"),
+            ("WyeCore.libs.WyeLib.makePickable",
+              ("Expr","frame.vars.tTags[0][frame.vars.tgCt[0]]"),
+              ("Expr","[obj._path]")),
+            ("Code","WyeCore.World.registerObjTag(frame.vars.tTags[0][frame.vars.tgCt[0]][0], frame)"),
+            ("Var=","frame.vars.tSegCt[0] += 1 #Next segment of this tentacle"),
+            ("Code","#loop to build next tentacle seg"),
+            ("IfGoTo","frame.vars.tSegCt[0] < 6","BldTentacle"),
+            ("Var=","frame.vars.tAngle[0] += math.pi/3."),
+            ("Var=","frame.vars.tCt[0] += 1 # Next tentacle"),
+            ("Code","#Loop to build next tentacle"),
+            ("IfGoTo","frame.vars.tCt[0] < 6","BldTentacles"),
+            ("Label","Drift"),
+            ("Code","import math"),
+            ("Code","frame.vars.body[0].setColor((frame.vars.color[0][0], frame.vars.color[0][1], frame.vars.color[0][2], 1))"),
+            ("Code","frame.vars.colorInc[0][0] *= 1 if 0 < frame.vars.color[0][0] + frame.vars.colorInc[0][0] < 1 else -1"),
+            ("Code","frame.vars.colorInc[0][1] *= 1 if 0< frame.vars.color[0][1] + frame.vars.colorInc[0][1] < 1 else -1"),
+            ("Code","frame.vars.colorInc[0][2] *= 1 if 0 < frame.vars.color[0][2] + frame.vars.colorInc[0][2] < 1 else -1"),
+            ("Code","frame.vars.color[0][0] += frame.vars.colorInc[0][0]"),
+            ("Code","frame.vars.color[0][1] += frame.vars.colorInc[0][1]"),
+            ("Code","frame.vars.color[0][2] += frame.vars.colorInc[0][2]"),
+            ("Code","#print('start jFish cycle, color', frame.vars.color[0])"),
+            ("Code","frame.vars.tAngle[0] = 0."),
+            ("Code","for tIx in range(6):"),
+            ("Code"," x = math.sin(frame.vars.tAngle[0])"),
+            ("Code"," y = math.cos(frame.vars.tAngle[0])"),
+            ("Code"," frame.vars.tAngle[0] += math.pi/3."),
+            ("Code"," color = [frame.vars.color[0][0], frame.vars.color[0][1], frame.vars.color[0][2]]"),
+            ("Code"," #print('start tentacle, color', color)"),
+            ("Code"," frame.vars.tColorInc[0] = [abs(frame.vars.tColorInc[0][0]), abs(frame.vars.tColorInc[0][1]), abs(frame.vars.tColorInc[0][2])]"),
+            ("Code"," for sIx in range(frame.vars.nSegs[0]):"),
+            ("Code","  frame.vars.segAngle[0] = frame.vars.segAngle[0] + frame.vars.dAngle[0] if frame.vars.segAngle[0] < 360 else 0"),
+            ("Code","  obj = frame.vars.t[0][tIx][sIx][0]"),
+            ("Code","  obj.setPos(x + math.sin(frame.vars.segAngle[0]+sIx*.25) * (sIx+1)*.05, y, -.5 - (sIx*.5))"),
+            ("Code","  #print('color', color)"),
+            ("Code","  obj.setColor((color[0], color[1], color[2], 1))"),
+            ("Code","  frame.vars.tColorInc[0][0] *= 1 if 0 < color[0] + frame.vars.tColorInc[0][0] < 1 else -1"),
+            ("Code","  frame.vars.tColorInc[0][1] *= 1 if 0 < color[1] + frame.vars.tColorInc[0][1] < 1 else -1"),
+            ("Code","  frame.vars.tColorInc[0][2] *= 1 if 0 < color[2] + frame.vars.tColorInc[0][2] < 1 else -1"),
+            ("Code","  color[0] += frame.vars.tColorInc[0][0]"),
+            ("Code","  color[1] += frame.vars.tColorInc[0][1]"),
+            ("Code","  color[2] += frame.vars.tColorInc[0][2]"))),
+        ("Move",
+          (
+            ("Label","Wait1Cycle"),
+            ("Label","Wait1Cycle"),
+            ("Label","Wait1Cycle"),
+            ("Label","Wait1Cycle"),
+            ("Code","#print('gObj', frame.vars.gObj[0])"),
+            ("Code","pos = frame.vars.gObj[0].getPos()"),
+            ("Code","pos[0]+=.001"),
+            ("Code","if pos[0] > 400:"),
+            ("Code"," pos[0] -= 800"),
+            ("Code","frame.vars.gObj[0].setPos(pos)"))))
+
+    def _build(rowRef):
+        # print("Build ",Jelly)
+        rowIxRef = [0]
+        return WyeCore.Utils.buildParallelText('FunFishLib', 'Jelly', FunFishLib.Jelly.codeDescr, FunFishLib.Jelly)
+
+    def start(stack):
+        return FunFishLib.FunFishLib_rt.Jelly_start_rt(stack)
+
+    def run(frame):
+        # print('Run 'Jelly)
+        frame.runParallel()
+
+  class Jelly1:
+    mode = Wye.mode.PARALLEL
     autoStart = False
     dataType = Wye.dType.NONE
     cType = Wye.cType.VERB
@@ -72,8 +203,7 @@ class FunFishLib:
             ("Var=","frame.vars.angle[0] += frame.vars.dAngle[0]"),
             ("Code","for ii in range(frame.vars.nSegs[0]):"),
             ("Code","  obj = frame.vars.t1[0][ii][0]"),
-            ("Code","  obj.setPos(1 + math.sin(frame.vars.angle[0]+ii*.25) * (ii+1)*.05, 0, -.5 - (ii*.5))"),
-            )),
+            ("Code","  obj.setPos(1 + math.sin(frame.vars.angle[0]+ii*.25) * (ii+1)*.05, 0, -.5 - (ii*.5))"))),
         ("CreateT2Stream",
           (
             ("Label","BldTentacles"),
@@ -94,7 +224,7 @@ class FunFishLib:
             ("Var=","frame.vars.angle[0] += frame.vars.dAngle[0]"),
             ("Code","for ii in range(frame.vars.nSegs[0]):"),
             ("Code","  obj = frame.vars.t2[0][ii][0]"),
-            ("Code","  obj.setPos(.5 + math.sin(frame.vars.angle[0]+ii*.25) * (ii+1)*.05, .866, -.5 - (ii*.5))"),)),
+            ("Code","  obj.setPos(.5 + math.sin(frame.vars.angle[0]+ii*.25) * (ii+1)*.05, .866, -.5 - (ii*.5))"))),
         ("CreateT3Stream",
           (
             ("Label","BldTentacles"),
@@ -115,7 +245,7 @@ class FunFishLib:
             ("Var=","frame.vars.angle[0] += frame.vars.dAngle[0]"),
             ("Code","for ii in range(frame.vars.nSegs[0]):"),
             ("Code","  obj = frame.vars.t3[0][ii][0]"),
-            ("Code","  obj.setPos(-.5 + math.sin(frame.vars.angle[0]+ii*.25) * (ii+1)*.05, .866, -.5 - (ii*.5))"),)),
+            ("Code","  obj.setPos(-.5 + math.sin(frame.vars.angle[0]+ii*.25) * (ii+1)*.05, .866, -.5 - (ii*.5))"))),
         ("CreateT4Stream",
           (
             ("Label","BldTentacles"),
@@ -136,7 +266,7 @@ class FunFishLib:
             ("Var=","frame.vars.angle[0] += frame.vars.dAngle[0]"),
             ("Code","for ii in range(frame.vars.nSegs[0]):"),
             ("Code","  obj = frame.vars.t4[0][ii][0]"),
-            ("Code","  obj.setPos(-1 + math.sin(frame.vars.angle[0]+ii*.25) * (ii+1)*.05, 0, -.5 - (ii*.5))"),)),
+            ("Code","  obj.setPos(-1 + math.sin(frame.vars.angle[0]+ii*.25) * (ii+1)*.05, 0, -.5 - (ii*.5))"))),
         ("CreateT5Stream",
           (
             ("Label","BldTentacles"),
@@ -157,7 +287,7 @@ class FunFishLib:
             ("Var=","frame.vars.angle[0] += frame.vars.dAngle[0]"),
             ("Code","for ii in range(frame.vars.nSegs[0]):"),
             ("Code","  obj = frame.vars.t5[0][ii][0]"),
-            ("Code","  obj.setPos(-.5 + math.sin(frame.vars.angle[0]+ii*.25) * (ii+1)*.05, -.8660, -.5 - (ii*.5))"),)),
+            ("Code","  obj.setPos(-.5 + math.sin(frame.vars.angle[0]+ii*.25) * (ii+1)*.05, -.8660, -.5 - (ii*.5))"))),
         ("CreateT6Stream",
           (
             ("Label","BldTentacles"),
@@ -178,22 +308,22 @@ class FunFishLib:
             ("Var=","frame.vars.angle[0] += frame.vars.dAngle[0]"),
             ("Code","for ii in range(frame.vars.nSegs[0]):"),
             ("Code","  obj = frame.vars.t6[0][ii][0]"),
-            ("Code","  obj.setPos(.51 + math.sin(frame.vars.angle[0]+ii*.25) * (ii+1)*.05, -.866, -.5 - (ii*.5))"),)),
+            ("Code","  obj.setPos(.51 + math.sin(frame.vars.angle[0]+ii*.25) * (ii+1)*.05, -.866, -.5 - (ii*.5))"))),
         ("Move",
           (
             ("Code","frame.vars.noop[0] = 0"),
             ("Label","Done"))))
 
     def _build(rowRef):
-        # print("Build ",Jelly)
+        # print("Build ",Jelly1)
         rowIxRef = [0]
-        return WyeCore.Utils.buildParallelText('FunFishLib', 'Jelly', FunFishLib.Jelly.codeDescr, FunFishLib.Jelly)
+        return WyeCore.Utils.buildParallelText('FunFishLib', 'Jelly1', FunFishLib.Jelly1.codeDescr, FunFishLib.Jelly1)
 
     def start(stack):
-        return FunFishLib.FunFishLib_rt.Jelly_start_rt(stack)
+        return FunFishLib.FunFishLib_rt.Jelly1_start_rt(stack)
 
     def run(frame):
-        # print('Run 'Jelly)
+        # print('Run 'Jelly1)
         frame.runParallel()
 
   class parallelWanderFish:
